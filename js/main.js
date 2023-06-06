60ed9a4b9dec490ccdcb21252929cf544c709658
Cardoso30S <85962499+Cardoso30S@users.noreply.github.com>
2023-06-05 23:56:16 -0300

    Atualizações site oficial, faltando apenas grupo 4

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function romantismo(){
+    alert("Redirecionando para o quiz...");
+    window.location.href = "https://wordwall.net/pt/embed/7060c422887d46d6b5f0257315f4579a?themeId=1&templateId=5&fontStackId=0%22"
+      
+}
+
+function modernismo(){
+    alert("Redirecionando para o quiz...");
+    window.location.href ="https://wordwall.net/pt/embed/aba16c2eab584f5899c40b15eee3ab15?themeId=1&templateId=5&fontStackId=12"
+}
+
+function realismo(){
+    alert("Redirecionando para o quiz...");
+    window.location.href= "https://wordwall.net/pt/embed/432f66c01c08444b9fbdbb4b178e5131?themeId=1&templateId=5&fontStackId=12"
+}
+
+function homepg(){
+    window.location.href= "index.html"
+}
+
+function mapaMental(){
+    alert("Redirecionando para mapa mental sobre Fuso Horário.");
+    window.location.href = "https://drive.google.com/file/d/1OPkP_k35tJqZvd-tf78XzpKZa6gqA2ef/preview"
+}
+
+function MapaMental(){
+    alert("Redirecionando para mapa mental sobre Fluxo Migratório.");
+    window.location.href = "https://drive.google.com/file/d/1XjUP38FCQTRzXoIYdifBC9M8JCEPtFf_/preview"
+}
+
